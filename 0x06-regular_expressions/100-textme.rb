6028001d6aee4c8f46aa3edccc05ceab5c0e4b3e
magezil <256@holbertonschool.com>
2018-02-20 00:43:39 +0000

    Corrected output format

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from:.\w*/).join.slice(5..-1)
-puts ARGV[0].scan(/to:.\w*/).join.slice(3..-1)
+print ARGV[0].scan(/from:.\w*/).join.slice(5..-1)
+print ","
+print ARGV[0].scan(/to:.\w*/).join.slice(3..-1)
+print ","
 puts ARGV[0].scan(/flags:(-?\d:?)(-?\d:?)(-?\d:?)(-?\d:?)(-?\d:?)/).join
