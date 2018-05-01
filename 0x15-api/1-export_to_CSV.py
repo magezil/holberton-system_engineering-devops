8e8ce3489d52b7ea15524e281377aa7ae32a33fa
magezil <256@holbertonschool.com>
2018-05-01 00:08:30 +0000

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+    Export TODO list data to CSV
+"""
+import csv
+import requests
+from sys import argv
+
+
+def get_completed(employee_id):
+    """
+        Script uses REST API to return information about TODO list progress
+        based on given employee ID
+
+        Output:
+            Employee NAME is done with tasks(NUMBER_DONE/TOTAL_NUMBER):
+                TASK_TITLE
+    """
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
+    response = requests.get(url).json()
+    name = response.get("name")
+
+    done = 0
+    tasks = []
+    total = 0
+    todo_url = 'https://jsonplaceholder.typicode.com/todos/'
+    for line in requests.get(todo_url).json():
+        if line.get('userId') == employee_id:
+            total += 1
+            if line.get('completed'):
+                done += 1
+                tasks.append(line.get('title'))
+
+    print("Employee {} is done with tasks({}/{}):".format(name, done, total))
+    for task in tasks:
+        print("    {}".format(task))
+
+
+def save_to_CSV(employee_id):
+    """
+        Export TODO list data to CSV for given employee_id
+
+        File name: USER_ID.csv
+
+        Format of file:
+            "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+    """
+    filename = "{}.csv".format(employee_id)
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
+    response = requests.get(url).json()
+    name = response.get("name")
+
+    todo_url = 'https://jsonplaceholder.typicode.com/todos/'
+    todo_response = requests.get(todo_url).json()
+    attrs = ["userId", "username", "completed", "title"]
+    with open(filename, 'w') as csvf:
+        writer = csv.DictWriter(csvf, fieldnames=attrs)
+        writer.writeheader()
+        for line in todo_response:
+            if line.get('userId') == employee_id:
+                line['username'] = name
+                del line['id']
+                writer.writerow(line)
+
+
+if __name__ == "__main__":
+    save_to_CSV(int(argv[1]))
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -3,3 +3,6 @@
 ## API
 ### 0-gather_data_from_an_API.py
 * Script uses REST API to return information about TODO list progress based on given employee ID
+### 1-export_to_CSV.py
+* Export data to CSV
+
