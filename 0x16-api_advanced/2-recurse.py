b4a6965427728d3e104f672fec32d03b3bd2465e
magezil <256@holbertonschool.com>
2018-05-05 00:40:39 +0000

    Remove loop inside recursive method

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -15,17 +15,30 @@ def recurse(subreddit, hot_list=[], last_id=None):
     url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit)
     if last_id is not None:
         url = "{}&after=t3_{}".format(url, last_id)
-        last_id = None
     headers = {'User-Agent': 'CustomClient/1.0'}
-    r = requests.get(url, headers=headers, allow_redirects=False).json()
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code != 200:
+        return None
+    r = r.json()
     if 'data' in r:
-        for post in r['data']['children']:
-            hot_list.append(post['data']['title'])
-            last_id = post['data']['id']
-        if last_id is not None:
-            hot_list.append(recurse(subreddit, hot_list, last_id))
+        fill_list(r['data']['children'], hot_list)
+        if not r['data']['children']:
+            return hot_list
+        last_id = r['data']['children'][-1]['data']['id']
+        hot_list.append(recurse(subreddit, hot_list, last_id))
         if hot_list[-1] is None:
             del hot_list[-1]
         return hot_list
     else:
         return None
+
+
+def fill_list(posts, hot_list):
+    """
+        Recursively fill hot_list from given data
+    """
+    if not posts:
+        return hot_list
+    hot_list.append(posts[0]['data']['title'])
+    fill_list(posts[1:], hot_list)
+    return hot_list
